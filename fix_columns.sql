130ee68a26feea676d6b5dfcd4ccfc85d039af92
vranasinghe <venujaranasinghe426@gmail.com>
2026-09-25 23:54:24 +0530

    Update fix_columns.sql

--- fix_columns.sql
+++ fix_columns.sql
@@ -1,6 +1,4 @@
 -- Add missing columns to products table
-ALTER TABLE products ADD COLUMN IF NOT EXISTS weight_g INT;
-ALTER TABLE products ADD COLUMN IF NOT EXISTS stock_quantity INT DEFAULT 100;
+
 
 -- Reload schema cache
-NOTIFY pgrst, 'reload schema';
